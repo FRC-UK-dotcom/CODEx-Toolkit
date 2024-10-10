9350d718c41793d42f3f4978d926a8ec3cd6fe4b
FRC-UK-dotcom <it@frc.org.uk>
2024-10-10 10:25:02 +0100

    Cleared all files from the main branch

--- xbrlanalyse/__init__.py
+++
@@ -1,5 +0,0 @@
-from .arelleloader import ArelleLoader
-from .concealedfacts import concealed_fact_count
-from .identifysoftware import identify_software
-from .hiddenfacts import hidden_facts
-from .util import is_url, url_path_join, load_json, local_file
